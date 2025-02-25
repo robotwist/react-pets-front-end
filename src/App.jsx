543afea6f649b5dcbe7a54107d9382d2c6fec5b8
Rob Wistrand <robwistrand@gmail.com>
2025-02-25 14:10:47 -0800

    daywork

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 // src/App.jsx
-
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList.jsx';
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+      const fetchedPets = await petService.index();
+      if (fetchedPets.err) {
+        throw new Error(fetchedPets.err);
+      }
+      setPets(fetchedPets);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+      fetchPets();
+  }, []);
+const handleSelect = (pet) => {
+    setSelected(pet);
+  };
+
+  return (
+    <>
+      <PetList pets={pets} handleSelect={handleSelect} />
+    </>
+  );
 };
 
 export default App;
